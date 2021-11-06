445b0cfa1b6d6b47b86fce59758f63f68e6670a7
Thanh Le <mihthanh27@gmail.com>
2021-11-06 13:05:48 +0700

    Add test for write command

--- destinations/streamr-destination/test/index.test.ts
+++ destinations/streamr-destination/test/index.test.ts
@@ -2,6 +2,7 @@ import {
   AirbyteConnectionStatus,
   AirbyteConnectionStatusMessage,
   AirbyteSpec,
+  AirbyteStateMessage,
 } from 'faros-airbyte-cdk';
 import fs from 'fs';
 import {getLocal} from 'mockttp';
@@ -9,7 +10,6 @@ import os from 'os';
 
 import {CLI, read} from './cli';
 import {tempConfig} from './temp';
-
 const privateKey =
   '9c9853f5ad682ae1552cf9bb205da37b704bc6d6105a94e190c5727d25e3e6cc';
 const streamId = '0x0d0102474519cd2fc1b3e3f962a87e39cbcbead2/test-streamr';
@@ -62,6 +62,33 @@ describe('index', () => {
       JSON.stringify(
         new AirbyteConnectionStatusMessage({
           status: AirbyteConnectionStatus.SUCCEEDED,
+        })
+      ) + os.EOL
+    );
+    expect(await cli.wait()).toBe(0);
+  });
+
+  test('indexing', async () => {
+    const catalogPath = '../sample_files/configured_catalog.json';
+    const recordPath = '../sample_files/records.jsonl';
+    const cli = await CLI.runWith([
+      'write',
+      '--config',
+      configPath,
+      '--catalog',
+      catalogPath,
+    ]);
+    const records = fs.readFileSync(recordPath);
+
+    cli.stdin.write(records);
+
+    expect(await read(cli.stderr)).toBe('');
+    expect(await read(cli.stdout)).toBe(
+      JSON.stringify(
+        new AirbyteStateMessage({
+          data: {
+            streamId: '_airbyte_raw_mytestsource__asana__users',
+          },
         })
       ) + os.EOL
     );
